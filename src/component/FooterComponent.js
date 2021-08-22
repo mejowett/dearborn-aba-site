fa0859db89c586679dc05e48cb96f90eddc0c757
mejowett <78455126+mejowett@users.noreply.github.com>
2021-08-22 12:04:18 -0400

    Version 1.0

--- src/component/FooterComponent.js
+++ src/component/FooterComponent.js
@@ -59,34 +59,6 @@ function Footer(props) {
                 in justo.
               </p>
             </div>
-            <div className="row">
-              <div class="col item social">
-                <a
-                  className="btn btn-social-icon btn-instagram"
-                  href="http://instagram.com/"
-                >
-                  <i className="fa fa-instagram" />
-                </a>{" "}
-                <a
-                  className="btn btn-social-icon btn-facebook"
-                  href="http://www.facebook.com/"
-                >
-                  <i className="fa fa-facebook" />
-                </a>{" "}
-                <a
-                  className="btn btn-social-icon btn-twitter"
-                  href="http://twitter.com/"
-                >
-                  <i className="fa fa-twitter" />
-                </a>{" "}
-                <a
-                  className="btn btn-social-icon btn-google"
-                  href="http://youtube.com/"
-                >
-                  <i className="fa fa-youtube" />
-                </a>
-              </div>
-            </div>
           </div>
         </div>
       </footer>
